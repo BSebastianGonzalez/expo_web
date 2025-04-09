edb13a4b23f91a69237017d29626a899a61d807d
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-04-08 20:52:17 -0500

    adding project structure

---
+++ src/main/java/co/ufps/expo/ExpoServicesApplication.java
@@ -0,0 +1,13 @@
+package co.ufps.expo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpoServicesApplication.class, args);
+	}
+
+}
